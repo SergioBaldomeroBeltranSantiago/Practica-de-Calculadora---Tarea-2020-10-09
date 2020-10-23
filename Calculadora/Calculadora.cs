5aa2435cb0dcf487a6efa1abe6e6c086a97afdca
sergiobaldomerobeltran@gmail.com <sergiobaldomerobeltran@gmail.com>
2020-10-23 11:55:44 -0700

    Corregido un error, experimentacion realizada y revertida

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,13 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculadora
@@ -117,7 +109,7 @@ namespace Calculadora
         {
             double resultado = Calcular(Leer());
             if(zerodiv==true){
-                Dísplay.Text = "Syntax Error."
+                Display.Text = "Syntax Error.";
             } else {
             Display.Text = Convert.ToString(resultado);
             }
